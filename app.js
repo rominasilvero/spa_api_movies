25ebc1a63daf59c1a308c903048aaa0af4b4e46a
Romina Silvero <rominasilveroiq@gmail.com>
2023-01-06 23:55:48 -0300

    Agrego version inicial proyecto API movies

---
+++ app.js
@@ -0,0 +1,52 @@
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () =>{
+    if (pagina < 1000){
+        pagina += 1;
+        cargarPeliculas();
+    }
+    
+
+});
+
+btnAnterior.addEventListener('click', () =>{
+    if (pagina > 1){
+        pagina -= 1;
+        cargarPeliculas();
+    }
+    
+
+});
+
+const cargarPeliculas = async() => {
+    try{
+        const respuesta = await fetch(`https:/api.themoviedb.org/3/movie/popular?api_key=e6e7c89bed94e89632cc1ab772009552&language=es-MX&page=${pagina}`);
+
+        console.log(respuesta);
+
+        //si la respuesta es correcta
+        if(respuesta.status === 200){
+        const datos = await respuesta.json();
+
+            let peliculas = '';
+            datos.results.forEach(pelicula => {
+                peliculas +=`
+                    <div class="pelicula">
+                        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                        <h3 class="titulo">${pelicula.title}</h3>
+                    </div>
+                    `;
+            });
+
+        document.getElementById('contenedor').innerHTML = peliculas;
+    }
+
+    } catch(error){
+        console.log(error);
+    }
+
+}
+
+cargarPeliculas();
